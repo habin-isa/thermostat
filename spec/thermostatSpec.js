c3e1c7d558ed17f7e57ddcd24fbb5f04ac6ebfaf
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-05-16 15:30:21 +0100

    Add up function to Thermostat

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -7,3 +7,10 @@ describe('Thermostat', function() {
     expect(thermostat.temperature).toEqual(20);
   });
 });
+
+describe('Up', function() {
+  it('should increase the temperate by 5', function() {
+    thermostat.up(5);
+    expect(thermostat.temperature).toEqual(25);
+  });
+});
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -3,3 +3,7 @@ const DEFAULT_TEMPERATURE = 20;
 function Thermostat() {
   this.temperature = DEFAULT_TEMPERATURE;
 };
+
+Thermostat.prototype.up = function(num) {
+  this.temperature += num
+}
